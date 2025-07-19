e2023bfd03474ba9573acfc97e13b365d01b30bf
rivst <me@riv.st>
2025-07-19 20:19:12 +0300

    Add content to index page

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("script.js");
   eleventyConfig.addPassthroughCopy("assets/fonts");
-  eleventyConfig.addPassthroughCopy("now.txt");
+  eleventyConfig.addPassthroughCopy("email.png");
   eleventyConfig.addFilter("post_date", (dateObj) => {
     return `${dateObj.toLocaleString("default", {
       dateStyle: "medium",
